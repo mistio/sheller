3d724c95010de7ffae91f8db9961861bc054721c
Dimitris Moraitis <dimo@mist.io>
2020-04-15 03:27:20 +0300

    Proxy websocket through ssh tunnel, using key from mongo

--- huproxy.go
+++ huproxy.go
@@ -15,17 +15,27 @@ package main
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/elliotchance/sshtunnel"
+	huproxy "github.com/google/huproxy/lib"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
-
-	huproxy "github.com/google/huproxy/lib"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/ssh"
 )
 
 var (
@@ -37,13 +47,106 @@ var (
 	upgrader websocket.Upgrader
 )
 
+// Key model
+type Key struct {
+	ID        string `bson:"_id, omitempty"`
+	Class     string `bson:"_cls"`
+	Name      string `bson:"name" json:"name"`
+	Owner     string `bson:"owner" json:"owner"`
+	Default   bool   `bson:"default" json:"default"`
+	Public    string `bson:"public" json:"public"`
+	Private   string `bson:"private" json:"private"`
+	OwnedBy   string `bson:"owned_by" json:"owned_by"`
+	CreatedBy string `bson:"created_by" json:"created_by"`
+}
+
 func handleProxy(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithCancel(r.Context())
 	defer cancel()
 
 	vars := mux.Vars(r)
+	proxy := vars["proxy"]
+	keyID := vars["key"]
 	host := vars["host"]
 	port := vars["port"]
+	expiry, _ := strconv.ParseInt(vars["expiry"], 10, 64)
+	mac := vars["mac"]
+
+	// Create a new HMAC by defining the hash type and the key (as byte array)
+	h := hmac.New(sha256.New, []byte(os.Getenv("SECRET")))
+
+	// Write Data to it
+	h.Write([]byte(proxy + "," + keyID + "," + host + "," + port + "," + vars["expiry"]))
+
+	// Get result and encode as hexadecimal string
+	sha := hex.EncodeToString(h.Sum(nil))
+
+	if sha != mac {
+		log.Println("HMAC mismatch")
+		return
+	}
+
+	if expiry < time.Now().Unix() {
+		log.Println("Session expired")
+		return
+	}
+
+	mctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(mctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	collection := client.Database("mist2").Collection("keys")
+	//objID, err := primitive.ObjectIDFromHex(keyID)
+	//if err != nil {
+	//	log.Println(keyID)
+	//	log.Fatal(err)
+	//}
+
+	key := Key{}
+
+	findResult := collection.FindOne(mctx, bson.M{"_id": keyID})
+	if err := findResult.Err(); err != nil {
+		log.Println(keyID)
+		log.Fatal(err)
+	}
+	err = findResult.Decode(&key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	priv, err := ssh.ParsePrivateKey([]byte(key.Private))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Setup the tunnel, but do not yet start it yet.
+	tunnel := sshtunnel.NewSSHTunnel(
+		// User and host of tunnel server, it will default to port 22
+		// if not specified.
+		proxy,
+		ssh.PublicKeys(priv), // 1. private key
+		host+":"+port,
+		"0",
+	)
+	// You can provide a logger for debugging, or remove this line to
+	// make it silent.
+	tunnel.Log = log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds)
+	// Start the server in the background. You will need to wait a
+	// small amount of time for it to bind to the localhost port
+	// before you can start sending connections.
+	go tunnel.Start()
+	time.Sleep(100 * time.Millisecond)
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -52,7 +155,7 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	s, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), *dialTimeout)
+	s, err := net.DialTimeout("tcp", net.JoinHostPort("127.0.0.1", strconv.Itoa(tunnel.Local.Port)), *dialTimeout)
 	if err != nil {
 		log.Println(err)
 		return
@@ -69,7 +172,8 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			if err != nil {
-				log.Fatalf("nextreader: %v", err)
+				log.Printf("nextreader: %v", err)
+				return
 			}
 			if mt != websocket.BinaryMessage {
 				log.Fatal("blah")
@@ -109,7 +213,7 @@ func main() {
 
 	log.Printf("huproxy %s", huproxy.Version)
 	m := mux.NewRouter()
-	m.HandleFunc("/proxy/{host}/{port}", handleProxy)
+	m.HandleFunc("/proxy/{proxy}/{key}/{host}/{port}/{expiry}/{mac}", handleProxy)
 	s := &http.Server{
 		Addr:           *listen,
 		Handler:        m,
