8af7f811ca7c85e74ebcb042978095225552f5d4
galexakhs97 <58263228+Looper2074@users.noreply.github.com>
2022-05-25 20:52:11 +0300

    remove debug messages when requesting a vault secret

--- util/secret/vault/vault.go
+++ util/secret/vault/vault.go
@@ -3,7 +3,6 @@ package vault
 import (
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
 	"time"
 )
@@ -25,14 +24,6 @@ func GetSecret(t Token, p SecretPath, expiry int64) (Secret, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Println("VAULT TOKEN")
-	log.Println(string(t))
-	log.Println("SECRET PATH")
-	log.Println(string(p))
-	log.Println("VAULT RESPONSE BODY:")
-	log.Printf("%s", resp.Body)
-	log.Println("VAULT RESPONSE STATUS:")
-	log.Println(resp.Status)
 	var r map[string]any
 	decoder := json.NewDecoder(resp.Body)
 	err = decoder.Decode(&r)
