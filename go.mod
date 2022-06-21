c7fe3f71164f839b49efef9b49556b95ca6663d1
George Alexakis <galexakhs97@gmail.com>
2022-06-21 11:13:56 +0000

    add lxd case

--- go.mod
+++ go.mod
@@ -6,10 +6,32 @@ require (
 	github.com/elliotchance/sshtunnel v1.3.1
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.5.0
-	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064
+	github.com/lxc/lxd v0.0.0-20220210225321-b29334016f17
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 )
 
 require (
-	golang.org/x/sys v0.0.0-20220329152356-43be30ef3008 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3 // indirect
+	github.com/go-macaroon-bakery/macaroonpb v1.0.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/juju/webbrowser v1.0.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/rogpeppe/fastuuid v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20220524220425-1d687d428aca // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/errgo.v1 v1.0.1 // indirect
+	gopkg.in/httprequest.v1 v1.2.1 // indirect
+	gopkg.in/inconshreveable/log15.v2 v2.0.0-20200109203555-b30bc20e4fd1 // indirect
+	gopkg.in/macaroon-bakery.v2 v2.3.0 // indirect
+	gopkg.in/macaroon.v2 v2.1.0 // indirect
 )
---
+++ lxd/lxd.go
@@ -0,0 +1,133 @@
+package lxd
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"sheller/machine"
+	"sheller/util/conceal"
+	"sheller/util/secret/vault"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/websocket"
+	lxd "github.com/lxc/lxd/client"
+	"github.com/lxc/lxd/shared/api"
+)
+
+func PrepareConnectionParameters(vars map[string]string) (string, *lxd.ConnectionArgs, error) {
+	decryptedMessage, err := conceal.Decrypt(vars["encrypted_msg"], "")
+	if err != nil {
+		return "", &lxd.ConnectionArgs{}, err
+	}
+	plaintextParts := strings.SplitN(decryptedMessage, ",", -1)
+	token := vault.Token(plaintextParts[0])
+	vault_addr := vault.SecretPath(plaintextParts[1])
+	vault_secret_engine_path := vault.SecretPath(plaintextParts[2])
+	key_path := vault.SecretPath(plaintextParts[3])
+	expiry, err := strconv.ParseInt(vars["expiry"], 10, 64)
+	if err != nil {
+		return "", &lxd.ConnectionArgs{}, err
+	}
+	secretPath := vault_addr + "/v1/" + vault_secret_engine_path + "/data/" + key_path
+	secretData, err := vault.GetSecret(token, secretPath, expiry)
+	if err != nil {
+		return "", &lxd.ConnectionArgs{}, err
+	}
+	ConnArgs := &lxd.ConnectionArgs{}
+	_, hasCaCert := secretData["ca_cert_file"]
+	if hasCaCert {
+		CaCert, ok := secretData["ca_cert_file"].(string)
+		if !ok {
+			return "", &lxd.ConnectionArgs{}, errors.New("can't read ca certificate")
+		}
+		ConnArgs.TLSCA = CaCert
+	}
+	ClientCert, ok := secretData["cert_file"].(string)
+	if !ok {
+		return "", &lxd.ConnectionArgs{}, errors.New("can't read client certificate")
+	}
+	ConnArgs.TLSClientCert = ClientCert
+	ClientKey, ok := secretData["key_file"].(string)
+	if !ok {
+		return "", &lxd.ConnectionArgs{}, errors.New("can't read client key")
+	}
+	ConnArgs.TLSClientKey = ClientKey
+	Host, ok := secretData["host"].(string)
+	if !ok {
+		return "", &lxd.ConnectionArgs{}, errors.New("can't read host")
+	}
+	Port, ok := secretData["port"].(string)
+	if !ok {
+		return "", &lxd.ConnectionArgs{}, errors.New("can't read port'")
+	}
+
+	url := fmt.Sprintf("https://%s:%s", Host, Port)
+	return url, ConnArgs, nil
+}
+func EstablishIOWebsockets(vars map[string]string) (*websocket.Conn, *websocket.Conn, error) {
+	url, ConnArgs, err := PrepareConnectionParameters(vars)
+	if err != nil {
+		return nil, nil, err
+	}
+	c, err := lxd.ConnectLXD(url, ConnArgs)
+	if err != nil {
+		return nil, nil, err
+	}
+	command := strings.Fields(os.Getenv("COMMAND"))
+	if len(command) == 0 {
+		command = []string{"/bin/bash"}
+	}
+	req := api.InstanceExecPost{
+		Command:     command,
+		Interactive: true,
+		WaitForWS:   true,
+	}
+
+	op, err := c.ExecInstance(vars["name"], req, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	secretFDS := op.Get().Metadata["fds"]
+	secret, ok := secretFDS.(map[string]any)
+	if !ok {
+		return nil, nil, errors.
+			New("operation metadata not in expected format")
+	}
+	secret_0, ok := secret["0"].(string)
+	if !ok {
+		return nil, nil, errors.
+			New("secret of websocket connection to bridge pty not in expected format")
+	}
+	secret_control, ok := secret["control"].(string)
+	if !ok {
+		return nil, nil, errors.
+			New("secret of websocket control connection not in expected format")
+	}
+	websocketStream, err := c.GetOperationWebsocket(op.Get().ID, secret_0)
+	if err != nil {
+		return nil, nil, err
+	}
+	controlConn, err := c.GetOperationWebsocket(op.Get().ID, secret_control)
+	if err != nil {
+		return nil, nil, err
+	}
+	return websocketStream, controlConn, nil
+}
+func ResizeTerminal(controlConn *websocket.Conn, size machine.TerminalSize) error {
+	msg := api.ContainerExecControl{}
+	msg.Command = "window-resize"
+	msg.Args = make(map[string]string)
+	msg.Args["width"] = strconv.Itoa(size.Width)
+	msg.Args["height"] = strconv.Itoa(size.Height)
+	buf, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	err = controlConn.WriteMessage(websocket.TextMessage, buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
