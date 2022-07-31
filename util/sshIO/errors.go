7a09070cb2ead99c56c1efd609d6ef25262e0dc2
George Alexakis <galexakhs97@gmail.com>
2022-07-31 11:38:01 +0000

    Modify sshIO errors

--- util/sshIO/errors.go
+++ util/sshIO/errors.go
@@ -3,11 +3,11 @@ package sshIO
 const packageName = "sshIO"
 
 var (
-	ErrWriteMessageToClient = packageName + ": failed writing websocket message to client"
-	ErrReadMessageType      = packageName + ": failed to detect whether the client was sending data or attempting to resize the terminal"
-	ErrReadClientMessage    = packageName + ": failed reading client's keystroke from websocket"
-	ErrReadRemoteStdout     = packageName + ": failed reading from pipe that is connected to the standard output of the remote host"
-	ErrWriteToRemoteStdin   = packageName + ": failed writing to pipe that is connected to the standard input of the remost host"
-	ErrResizeTerminal       = packageName + ": failed resizing terminal"
-	ErrSendCloseMessage     = packageName + ": failed sending websocket Close Message to client"
+	ErrWriteMessageToClient = packageName + ": writing websocket message to client failed"
+	ErrReadMessageType      = packageName + ": failed to detect whether the client was sending data or attempting to resize the terminal "
+	ErrReadClientMessage    = packageName + ": reading client's keystroke from websocket failed"
+	ErrReadRemoteStdout     = packageName + ": reading from pipe that is connected to the standard output of the remote host failed"
+	ErrWriteToRemoteStdin   = packageName + ": writing to pipe that is connected to the standard input of the remost host failed"
+	ErrResizeTerminal       = packageName + ": resizing terminal failed"
+	ErrSendCloseMessage     = packageName + ": sending websocket Close Message to client failed"
 )
