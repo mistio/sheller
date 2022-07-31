a940a38b15eebc0ad86fc55c2a348f7c82faacf0
George Alexakis <galexakhs97@gmail.com>
2022-07-31 11:38:16 +0000

    Modify websocketIO errors

--- util/websocketIO/errors.go
+++ util/websocketIO/errors.go
@@ -3,11 +3,11 @@ package websocketIO
 const packageName = "websocketIO"
 
 var (
-	ErrWriteMessageToClient = packageName + ": failed writing websocket message to client"
+	ErrWriteMessageToClient = packageName + ": writing websocket message to client failed"
 	ErrReadMessageType      = packageName + ": failed to detect whether the client was sending data or attempting to resize the terminal"
-	ErrReadClientMessage    = packageName + ": failed reading client's keystroke from websocket"
-	ErrReadHostMessage      = packageName + ": failed reading host's websocket message"
-	ErrWriteToHost          = packageName + ": failed writing websocket message to host"
-	ErrResizeTerminal       = packageName + ": failed resizing terminal"
-	ErrSendCloseMessage     = packageName + ": failed sending websocket Close Message to client"
+	ErrReadClientMessage    = packageName + ": reading client's keystroke from websocket failed"
+	ErrReadHostMessage      = packageName + ": reading host's websocket message failed"
+	ErrWriteToHost          = packageName + ": writing websocket message to host failed"
+	ErrResizeTerminal       = packageName + ": resizing terminal failed"
+	ErrSendCloseMessage     = packageName + ": sending websocket Close Message to client failed"
 )
